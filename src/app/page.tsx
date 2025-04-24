4d6fdba64a0226314cfccd0d08054e6c5eb056c8
Al Zilqad Tonoy <ttonoyy@gmail.com>
2025-04-24 19:11:54 +0600

    Initializing the base project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
+      <h1 className="text-5xl text-white font-bold mb-8 animate-pulse">
+        Coming Soon
+      </h1>
+      <p className="text-white text-lg mb-8">Under Construction. Stay tuned !</p>
+    </div>
+  );
+}
